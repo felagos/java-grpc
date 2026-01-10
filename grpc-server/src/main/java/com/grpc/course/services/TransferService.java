813073c380fff25666b59c41535386b450f07aae
Felipe Lagos <felagossanchez@gmail.com>
2026-01-10 17:45:08 -0300

    feat: Refactor TransferService to use TransferRequestHandler for processing transfers

--- grpc-server/src/main/java/com/grpc/course/services/TransferService.java
+++ grpc-server/src/main/java/com/grpc/course/services/TransferService.java
@@ -3,13 +3,21 @@ package com.grpc.course.services;
 import com.grpc.course.TransferRequest;
 import com.grpc.course.TransferResponse;
 import com.grpc.course.TransferServiceGrpc;
+import com.grpc.course.handlers.TransferRequestHandler;
+import com.grpc.course.repository.AccountRepository;
 
 import io.grpc.stub.StreamObserver;
 
 public class TransferService extends TransferServiceGrpc.TransferServiceImplBase {
 
+    private final AccountRepository accountRepository;
+
+    public TransferService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
     @Override
     public StreamObserver<TransferRequest> transfer(StreamObserver<TransferResponse> responseObserver) {
-        return super.transfer(responseObserver);
+        return new TransferRequestHandler(responseObserver, accountRepository);
     }
 }
